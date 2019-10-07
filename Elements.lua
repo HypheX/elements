dce9dd52b4e52ed024d3c3f4515c31528aa4b28d
Luis <luisalberto.fierrosalinas.1c@gmail.com>
2019-10-07 09:53:16 -0600

    Create Elements.lua

---
+++ Elements.lua
@@ -0,0 +1,264 @@
+local Elements = {}
+
+
+--------------------------------------------------FIRST PERIOD--------------------------------------------------
+
+Elements[1] = {
+	Position = {1,1},
+	Symbol = "H",
+	Name = "Hydrogen"
+}
+
+Elements[2] = {
+	Position = {18,1},
+	Symbol = "He",
+	Name = "Helium"
+}
+
+--------------------------------------------------SECOND PERIOD--------------------------------------------------
+
+Elements[3] = {
+	Position = {1,2},
+	Symbol = "Li",
+	Name = "Lithium"
+}
+
+Elements[4] = {
+	Position = {2,2},
+	Symbol = "Be",
+	Name = "Beryllium"
+}
+
+Elements[5] = {
+	Position = {13,2},
+	Symbol = "B",
+	Name = "Boron"
+}
+
+Elements[6] = {
+	Position = {14,2},
+	Symbol = "C",
+	Name = "Carbon"
+}
+
+Elements[7] = {
+	Position = {15,2},
+	Symbol = "N",
+	Name = "Nitrogen"
+}
+
+Elements[8] = {
+	Position = {16,2},
+	Symbol = "O",
+	Name = "Oxygen"
+}
+
+Elements[9] = {
+	Position = {17,2},
+	Symbol = "F",
+	Name = "Fluorine"
+}
+
+Elements[10] = {
+	Position = {18,2},
+	Symbol = "Ne",
+	Name = "Neon"
+}
+
+--------------------------------------------------THIRD PERIOD--------------------------------------------------
+
+Elements[11] = {
+	Position = {1,3},
+	Symbol = "Na",
+	Name = "Sodium"
+}
+
+Elements[12] = {
+	Position = {2,3},
+	Symbol = "Mg",
+	Name = "Magnesium"
+}
+
+Elements[13] = {
+	Position = {13,3},
+	Symbol = "Al",
+	Name = "Aluminum"
+}
+
+Elements[14] = {
+	Position = {14,3},
+	Symbol = "Si",
+	Name = "Silicon"
+}
+
+Elements[15] = {
+	Position = {15,3},
+	Symbol = "P",
+	Name = "Phosphorous"
+}
+
+Elements[16] = {
+	Position = {16,3},
+	Symbol = "S",
+	Name = "Sulfur"
+}
+
+Elements[17] = {
+	Position = {17,3},
+	Symbol = "Cl",
+	Name = "Chlorine"
+}
+
+Elements[18] = {
+	Position = {18,3},
+	Symbol = "Ar",
+	Name = "Argon"
+}
+
+--------------------------------------------------FOURTH PERIOD--------------------------------------------------
+
+Elements[19] = {
+	Position = {1,4},
+	Symbol = "K",
+	Name = "Potassium"
+}
+
+Elements[20] = {
+	Position = {2,4},
+	Symbol = "Ca",
+	Name = "Calcium"
+}
+
+Elements[21] = {
+	Position = {3,4},
+	Symbol = "Sc",
+	Name = "Scandium"
+}
+
+Elements[22] = {
+	Position = {4,4},
+	Symbol = "Ti",
+	Name = "Titanium"
+}
+
+Elements[23] = {
+	Position = {5,4},
+	Symbol = "V",
+	Name = "Vanadium"
+}
+
+Elements[24] = {
+	Position = {6,4},
+	Symbol = "Cr",
+	Name = "Chromium"
+}
+
+Elements[25] = {
+	Position = {7,4},
+	Symbol = "Mn",
+	Name = "Manganese"
+}
+
+Elements[26] = {
+	Position = {8,4},
+	Symbol = "Fe",
+	Name = "Iron"
+}
+
+Elements[27] = {
+	Position = {9,4},
+	Symbol = "Co",
+	Name = "Cobalt"
+}
+
+Elements[28] = {
+	Position = {10,4},
+	Symbol = "Ni",
+	Name = "Nickel"
+}
+
+Elements[29] = {
+	Position = {11,4},
+	Symbol = "Cu",
+	Name = "Copper"
+}
+
+Elements[30] = {
+	Position = {12,4},
+	Symbol = "Zn",
+	Name = "Zinc"
+}
+
+Elements[31] = {
+	Position = {13,4},
+	Symbol = "Ga",
+	Name = "Gallium"
+}
+
+Elements[32] = {
+	Position = {14,4},
+	Symbol = "Ge",
+	Name = "Germanium"
+}
+
+Elements[33] = {
+	Position = {15,4},
+	Symbol = "As",
+	Name = "Arsenic"
+}
+
+Elements[34] = {
+	Position = {16,4},
+	Symbol = "Se",
+	Name = "Selenium"
+}
+
+Elements[35] = {
+	Position = {17,4},
+	Symbol = "Br",
+	Name = "Bromine"
+}
+
+Elements[36] = {
+	Position = {18,4},
+	Symbol = "Kr",
+	Name = "Krypton"
+}
+
+--------------------------------------------------FIFTH PERIOD--------------------------------------------------
+
+Elements[37] = {
+	Position = {1,5},
+	Symbol = "Rb",
+	Name = "Rubidium"
+}
+
+Elements[38] = {
+	Position = {2,5},
+	Symbol = "Sr",
+	Name = "Strontium"
+}
+
+Elements[39] = {
+	Position = {3,5},
+	Symbol = "Y",
+	Name = "Yttrium"
+}
+
+Elements[40] = {
+	Position = {4,5},
+	Symbol = "Zr",	
+	Name = "Zirconium"
+}
+
+Elements[41] = {
+	Position = {5,5},
+	Symbol = "Nb",
+	Name = "Niobium"
+}
+
+Elements[42] = {
+	Position = {6,5},
+	Symbol = "Mo",
+	Name = "Molybdenum"
+}
