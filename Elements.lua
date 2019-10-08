1e61cf6096339d5264ee14d0214aba765b506fc8
Luis <luisalberto.fierrosalinas.1c@gmail.com>
2019-10-07 21:59:19 -0600

    Add the rest of the elements

--- Elements.lua
+++ Elements.lua
@@ -6,13 +6,15 @@ local Elements = {}
 Elements[1] = {
 	Position = {1,1},
 	Symbol = "H",
-	Name = "Hydrogen"
+	Name = "Hydrogen",
+	Classification = "Nonmetal"
 }
 
 Elements[2] = {
 	Position = {18,1},
 	Symbol = "He",
-	Name = "Helium"
+	Name = "Helium",
+	Classification = "Noble gas"
 }
 
 --------------------------------------------------SECOND PERIOD--------------------------------------------------
@@ -20,49 +22,57 @@ Elements[2] = {
 Elements[3] = {
 	Position = {1,2},
 	Symbol = "Li",
-	Name = "Lithium"
+	Name = "Lithium",
+	Classification = "Alkali metal"
 }
 
 Elements[4] = {
 	Position = {2,2},
 	Symbol = "Be",
-	Name = "Beryllium"
+	Name = "Beryllium",
+	Classification = "Alkaline earth metal"
 }
 
 Elements[5] = {
 	Position = {13,2},
 	Symbol = "B",
-	Name = "Boron"
+	Name = "Boron",
+	Classification = "Metalloid"
 }
 
 Elements[6] = {
 	Position = {14,2},
 	Symbol = "C",
-	Name = "Carbon"
+	Name = "Carbon",
+	Classification = "Nonmetal"
 }
 
 Elements[7] = {
 	Position = {15,2},
 	Symbol = "N",
-	Name = "Nitrogen"
+	Name = "Nitrogen",
+	Classification = "Nonmetal"
 }
 
 Elements[8] = {
 	Position = {16,2},
 	Symbol = "O",
-	Name = "Oxygen"
+	Name = "Oxygen",
+	Classification = "Nonmetal"
 }
 
 Elements[9] = {
 	Position = {17,2},
 	Symbol = "F",
-	Name = "Fluorine"
+	Name = "Fluorine",
+	Classification = "Nonmetal"
 }
 
 Elements[10] = {
 	Position = {18,2},
 	Symbol = "Ne",
-	Name = "Neon"
+	Name = "Neon",
+	Classification = "Noble gas"
 }
 
 --------------------------------------------------THIRD PERIOD--------------------------------------------------
@@ -70,49 +80,57 @@ Elements[10] = {
 Elements[11] = {
 	Position = {1,3},
 	Symbol = "Na",
-	Name = "Sodium"
+	Name = "Sodium",
+	Classification = "Alkali metal"
 }
 
 Elements[12] = {
 	Position = {2,3},
 	Symbol = "Mg",
-	Name = "Magnesium"
+	Name = "Magnesium",
+	Classification = "Alkaline earth metal"
 }
 
 Elements[13] = {
 	Position = {13,3},
 	Symbol = "Al",
-	Name = "Aluminum"
+	Name = "Aluminum",
+	Classification = "Metal"
 }
 
 Elements[14] = {
 	Position = {14,3},
 	Symbol = "Si",
-	Name = "Silicon"
+	Name = "Silicon",
+	Classification = "Metalloid"
 }
 
 Elements[15] = {
 	Position = {15,3},
 	Symbol = "P",
-	Name = "Phosphorous"
+	Name = "Phosphorous",
+	Classification = "Nonmetal"
 }
 
 Elements[16] = {
 	Position = {16,3},
 	Symbol = "S",
-	Name = "Sulfur"
+	Name = "Sulfur",
+	Classification = "Nonmetal"
 }
 
 Elements[17] = {
 	Position = {17,3},
 	Symbol = "Cl",
-	Name = "Chlorine"
+	Name = "Chlorine",
+	Classification = "Nonmetal"
 }
 
 Elements[18] = {
 	Position = {18,3},
 	Symbol = "Ar",
-	Name = "Argon"
+	Name = "Argon",
+	Classification = "Noble gas"
 }
 
 --------------------------------------------------FOURTH PERIOD--------------------------------------------------
@@ -120,109 +138,127 @@ Elements[18] = {
 Elements[19] = {
 	Position = {1,4},
 	Symbol = "K",
-	Name = "Potassium"
+	Name = "Potassium",
+	Classification = "Alkali metal"
 }
 
 Elements[20] = {
 	Position = {2,4},
 	Symbol = "Ca",
-	Name = "Calcium"
+	Name = "Calcium",
+	Classification = "Alkaline earth metal"
 }
 
 Elements[21] = {
 	Position = {3,4},
 	Symbol = "Sc",
-	Name = "Scandium"
+	Name = "Scandium",
+	Classification = "Transition metal"
 }
 
 Elements[22] = {
 	Position = {4,4},
 	Symbol = "Ti",
-	Name = "Titanium"
+	Name = "Titanium",
+	Classification = "Transition metal"
 }
 
 Elements[23] = {
 	Position = {5,4},
 	Symbol = "V",
-	Name = "Vanadium"
+	Name = "Vanadium",
+	Classification = "Transition metal"
 }
 
 Elements[24] = {
 	Position = {6,4},
 	Symbol = "Cr",
-	Name = "Chromium"
+	Name = "Chromium",
+	Classification = "Transition metal"
 }
 
 Elements[25] = {
 	Position = {7,4},
 	Symbol = "Mn",
-	Name = "Manganese"
+	Name = "Manganese",
+	Classification = "Transition metal"
 }
 
 Elements[26] = {
 	Position = {8,4},
 	Symbol = "Fe",
-	Name = "Iron"
+	Name = "Iron",
+	Classification = "Transition metal"
 }
 
 Elements[27] = {
 	Position = {9,4},
 	Symbol = "Co",
-	Name = "Cobalt"
+	Name = "Cobalt",
+	Classification = "Transition metal"
 }
 
 Elements[28] = {
 	Position = {10,4},
 	Symbol = "Ni",
-	Name = "Nickel"
+	Name = "Nickel",
+	Classification = "Transition metal"
 }
 
 Elements[29] = {
 	Position = {11,4},
 	Symbol = "Cu",
-	Name = "Copper"
+	Name = "Copper",
+	Classification = "Transition metal"
 }
 
 Elements[30] = {
 	Position = {12,4},
 	Symbol = "Zn",
-	Name = "Zinc"
+	Name = "Zinc",
+	Classification = "Transition metal"
 }
 
 Elements[31] = {
 	Position = {13,4},
 	Symbol = "Ga",
-	Name = "Gallium"
+	Name = "Gallium",
+	Classification = "Metal"
 }
 
 Elements[32] = {
 	Position = {14,4},
 	Symbol = "Ge",
-	Name = "Germanium"
+	Name = "Germanium",
+	Classification = "Metalloid"
 }
 
 Elements[33] = {
 	Position = {15,4},
 	Symbol = "As",
-	Name = "Arsenic"
+	Name = "Arsenic",
+	Classification = "Metalloid"
 }
 
 Elements[34] = {
 	Position = {16,4},
 	Symbol = "Se",
-	Name = "Selenium"
+	Name = "Selenium",
+	Classification = "Nonmetal"
 }
 
 Elements[35] = {
 	Position = {17,4},
 	Symbol = "Br",
-	Name = "Bromine"
+	Name = "Bromine",
+	Classification = "Nonmetal"
 }
 
 Elements[36] = {
 	Position = {18,4},
 	Symbol = "Kr",
-	Name = "Krypton"
+	Name = "Krypton",
+	Classification = "Noble gas"
 }
 
 --------------------------------------------------FIFTH PERIOD--------------------------------------------------
@@ -230,35 +266,587 @@ Elements[36] = {
 Elements[37] = {
 	Position = {1,5},
 	Symbol = "Rb",
-	Name = "Rubidium"
+	Name = "Rubidium",
+	Classification = "Alkali metal"
 }
 
 Elements[38] = {
 	Position = {2,5},
 	Symbol = "Sr",
-	Name = "Strontium"
+	Name = "Strontium",
+	Classification = "Alkaline earth metal"
 }
 
 Elements[39] = {
 	Position = {3,5},
 	Symbol = "Y",
-	Name = "Yttrium"
+	Name = "Yttrium",
+	Classification = "Transition metal"
 }
 
 Elements[40] = {
 	Position = {4,5},
 	Symbol = "Zr",	
-	Name = "Zirconium"
+	Name = "Zirconium",
+	Classification = "Transition metal"
 }
 
 Elements[41] = {
 	Position = {5,5},
 	Symbol = "Nb",
-	Name = "Niobium"
+	Name = "Niobium",
+	Classification = "Transition metal"
 }
 
 Elements[42] = {
 	Position = {6,5},
 	Symbol = "Mo",
-	Name = "Molybdenum"
+	Name = "Molybdenum",
+	Classification = "Transition metal"
+}
+
+Elements[43] = {
+	Position = {7,5},
+	Symbol = "Te",
+	Name = "Technetium",
+	Classification = "Transition metal"
+}
+
+Elements[44] = {
+	Position = {8,5},
+	Symbol = "Ru",
+	Name = "Ruthenium",
+	Classification = "Transition metal"
+}
+
+Elements[45] = {
+	Position = {9,5},
+	Symbol = "Rh",
+	Name = "Rhodium",
+	Classification = "Transition metal"
+}
+
+Elements[46] = {
+	Position = {10,5},
+	Symbol = "Pd",
+	Name = "Palladium",
+	Classification = "Transition metal"
+}
+
+Elements[47] = {
+	Position = {11,5},
+	Symbol = "Ag",
+	Name = "Silver",
+	Classification = "Transition metal"
+}
+
+Elements[48] = {
+	Position = {12,5},
+	Symbol = "Cd",
+	Name = "Cadmium",
+	Classification = "Transition metal"
+}
+
+Elements[49] = {
+	Position = {13,5},
+	Symbol = "In",
+	Name = "Indium",
+	Classification = "Metal"
+}
+
+Elements[50] = {
+	Position = {14,5},
+	Symbol = "Sn",
+	Name = "Tin",
+	Classification = "Metal"
+	
+}
+
+Elements[51] = {
+	Position = {15,5},
+	Symbol = "Sb",
+	Name = "Antimony",
+	Classification = "Metalloid"
+}
+
+Elements[52] = {
+	Position = {16,5},
+	Symbol = "Te",
+	Name = "Tellerium",
+	Classification = "Metalloid"
+}
+
+Elements[53] = {
+	Position = {17,5},
+	Symbol = "I",
+	Name = "Iodine",
+	Classification = "Nonmetal"
+}
+
+Elements[54] = {
+	Position = {18,5},
+	Symbol = "Xe",
+	Name = "Xenon",
+	Classification = "Noble gas"
+}
+
+--------------------------------------------------SIXTH PERIOD--------------------------------------------------
+
+Elements[55] = {
+	Position = {1,6},
+	Symbol = "Cs",
+	Name = "Caesium",
+	Classification = "Alkali metal"
+}
+
+Elements[56] = {
+	Position = {2,6},
+	Symbol = "Ba",
+	Name = "Barium",
+	Classification = "Alkaline earth metal"
+}
+
+Elements[57] = {
+	Position = {3,6},
+	Symbol = "La",
+	Name = "Lanthanum",
+	Classification = "Transition metal"
+}
+
+Elements[58] = {
+	Position = {4,9},
+	Symbol = "Ce",
+	Name = "Cerium",
+	Classification = "Lanthanoid"
+}
+
+Elements[59] = {
+	Position = {5,9},
+	Symbol = "Pr",
+	Name = "Praseodymium",
+	Classification = "Lanthanoid"
+}
+
+Elements[60] = {
+	Position = {6,9},
+	Symbol = "Nd",
+	Name = "Neodymium",
+	Classification = "Lanthanoid"
+}
+
+Elements[61] = {
+	Position = {7,9},
+	Symbol = "Pm",
+	Name = "Promethium",
+	Classification = "Lanthanoid"
+}
+
+Elements[62] = {
+	Position = {8,9},
+	Symbol = "Sm",
+	Name = "Samarium",
+	Classification = "Lanthanoid"
+}
+
+Elements[63] = {
+	Position = {9,9},
+	Symbol = "Eu",
+	Name = "Europium",
+	Classification = "Lanthanoid"
+}
+
+Elements[64] = {
+	Position = {10,9},
+	Symbol = "Gd",
+	Name = "Gadolinium",
+	Classification = "Lanthanoid"
+}
+
+Elements[65] = {
+	Position = {11,9},
+	Symbol = "Tb",
+	Name = "Terbium",
+	Classification = "Lanthanoid"
+}
+
+Elements[66] = {
+	Position = {12,9},
+	Symbol = "Dy",
+	Name = "Dysprosium",
+	Classification = "Lanthanoid"
+}
+
+Elements[67] = {
+	Position = {13,9},
+	Symbol = "Ho",
+	Name = "Holmium",
+	Classification = "Lanthanoid"
+}
+
+Elements[68] = {
+	Position = {14,9},
+	Symbol = "Er",
+	Name = "Erbium",
+	Classification = "Lanthanoid"
+}
+
+Elements[69] = {
+	Position = {15,9},
+	Symbol = "Tm",
+	Name = "Thulium",
+	Classification = "Lanthanoid"
+}
+
+Elements[70] = {
+	Position = {16,9},
+	Symbol = "Yb",
+	Name = "Ytterbium",
+	Classification = "Lanthanoid"
+}
+
+Elements[71] = {
+	Position = {17,9},
+	Symbol = "Lu",
+	Name = "Lutetium",
+	Classification = "Lanthanoid"
+}
+
+Elements[72] = {
+	Position = {4,6},
+	Symbol = "Hf",
+	Name = "Hafnium",
+	Classification = "Transition metal"
+}
+
+Elements[73] = {
+	Position = {5,6},
+	Symbol = "Ta",
+	Name = "Tantalum",
+	Classification = "Transition metal"
+}
+
+Elements[74] = {
+	Position = {6,6},
+	Symbol = "W",
+	Name = "Tungsten",
+	Classification = "Transition metal"
+}
+
+Elements[75] = {
+	Position = {7,6},
+	Symbol = "Re",
+	Name = "Rhenium",
+	Classification = "Transition metal"
+}
+
+Elements[76] = {
+	Position = {8,6},
+	Symbol = "Os",
+	Name = "Osmium",
+	Classification = "Transition metal"
+}
+
+Elements[77] = {
+	Position = {9,6},
+	Symbol = "Ir",
+	Name = "Iridium",
+	Classification = "Transition metal"
+}
+
+Elements[78] = {
+	Position = {10,6},
+	Symbol = "Pt",
+	Name = "Platinum",
+	Classification = "Transition metal"
+}
+
+Elements[79] = {
+	Position = {11,6},
+	Symbol = "Au",
+	Name = "Gold",
+	Classification = "Transition metal"
+}
+
+Elements[80] = {
+	Position = {12,6},
+	Symbol = "Hg",
+	Name = "Mercury",
+	Classification = "Transition metal"
+}
+
+Elements[81] = {
+	Position = {13,6},
+	Symbol = "Tl",
+	Name = "Thallium",
+	Classification = "Metal"
+}
+
+Elements[82] = {
+	Position = {14,6},
+	Symbol = "Pb",
+	Name = "Lead",
+	Classification = "Metal"
+}
+
+Elements[83] = {
+	Position = {15,6},
+	Symbol = "Bi",
+	Name = "Bismuth",
+	Classification = "Metal"
+}
+
+Elements[84] = {
+	Position = {16,6},
+	Symbol = "Po",
+	Name = "Polonium",
+	Classification = "Metal"
+}
+
+Elements[85] = {
+	Position = {17,6},
+	Symbol = "At",
+	Name = "Astatine",
+	Classification = "Metalloid"
+}
+
+Elements[86] = {
+	Position = {18,6},
+	Symbol = "Rn",
+	Name = "Radon",
+	Classification = "Noble gas"
+}
+
+Elements[86] = {
+	Position = {18,6},
+	Symbol = "Rn",
+	Name = "Radon",
+	Classification = "Noble gas"
+}
+
+--------------------------------------------------SEVENT PERIOD--------------------------------------------------
+
+Elements[87] = {
+	Position = {1,7},
+	Symbol = "Fr",
+	Name = "Francium",
+	Classification = "Alkali metal"
+}
+
+Elements[88] = {
+	Position = {2,7},
+	Symbol = "Ra",
+	Name = "Radium",
+	Classification = "Alkaline earth metal"
+}
+
+Elements[89] = {
+	Position = {3,7},
+	Symbol = "Ac",
+	Name = "Actinium",
+	Classification = "Transition metal"
+}
+
+Elements[90] = {
+	Position = {4,10},
+	Symbol = "Th",
+	Name = "Thorium",
+	Classification = "Actinoid"
+}
+
+Elements[91] = {
+	Position = {5,10},
+	Symbol = "Pa",
+	Name = "Protactinium",
+	Classification = "Actinoid"
+}
+
+Elements[92] = {
+	Position = {6,10},
+	Symbol = "U",
+	Name = "Uranium",
+	Classification = "Actinoid"
+}
+
+Elements[93] = {
+	Position = {7,10},
+	Symbol = "Np",
+	Name = "Neptunium",
+	Classification = "Actinoid"
+}
+
+Elements[94] = {
+	Position = {8,10},
+	Symbol = "Pu",
+	Name = "Plutonium",
+	Classification = "Actinoid"
+}
+
+Elements[95] = {
+	Position = {9,10},
+	Symbol = "Am",
+	Name = "Americium",
+	Classification = "Actinoid"
+}
+
+Elements[96] = {
+	Position = {10,10},
+	Symbol = "Cm",
+	Name = "Curium",
+	Classification = "Actinoid"
+}
+
+Elements[97] = {
+	Position = {11,10},
+	Symbol = "Bk",
+	Name = "Berkelium",
+	Classification = "Actinoid"
+}
+
+Elements[98] = {
+	Position = {12,10},
+	Symbol = "Cf",
+	Name = "Californium",
+	Classification = "Actinoid"
+}
+
+Elements[99] = {
+	Position = {13,10},
+	Symbol = "Es",
+	Name = "Einsteinium",
+	Classification = "Actinoid"
+}
+
+Elements[100] = {
+	Position = {14,10},
+	Symbol = "Fm",
+	Name = "Fermium",
+	Classification = "Actinoid"
+}
+
+Elements[101] = {
+	Position = {15,10},
+	Symbol = "Md",
+	Name = "Mendelevium",
+	Classification = "Actinoid"
+}
+
+Elements[102] = {
+	Position = {16,10},
+	Symbol = "No",
+	Name = "Nobelium",
+	Classification = "Actinoid"
+}
+
+
+Elements[103] = {
+	Position = {17,10},
+	Symbol = "Lr",
+	Name = "Lawrencium",
+	Classification = "Actinoid"
+}
+
+
+Elements[104] = {
+	Position = {4,7},
+	Symbol = "Rf",
+	Name = "Rutherfordium",
+	Classification = "Transition metal"
+}
+
+Elements[105] = {
+	Position = {5,7},
+	Symbol = "Db",
+	Name = "Dubnium",
+	Classification = "Transition metal"
+}
+
+Elements[106] = {
+	Position = {6,7},
+	Symbol = "Sg",
+	Name = "Seaborgium",
+	Classification = "Transition metal"
+}
+
+Elements[107] = {
+	Position = {7,7},
+	Symbol = "Bh",
+	Name = "Bohrium",
+	Classification = "Transition metal"
+}
+
+Elements[108] = {
+	Position = {8,7},
+	Symbol = "Hs",
+	Name = "Hassium",
+	Classification = "Transition metal"
+}
+
+Elements[109] = {
+	Position = {9,7},
+	Symbol = "Mt",
+	Name = "Meitnerium",
+	Classification = "Unknown"
+}
+
+Elements[110] = {
+	Position = {10,7},
+	Symbol = "Ds",
+	Name = "Darmstadtium",
+	Classification = "Unknown"
+}
+
+Elements[111] = {
+	Position = {11,7},
+	Symbol = "Rg",
+	Name = "Roentgenium",
+	Classification = "Unknown"
+}
+
+Elements[112] = {
+	Position = {12,7},
+	Symbol = "Cn",
+	Name = "Copernicium",
+	Classification = "Transition metal"
+}
+
+Elements[113] = {
+	Position = {13,7},
+	Symbol = "Nh",
+	Name = "Nihonium",
+	Classification = "Unknown"
+}
+
+Elements[114] = {
+	Position = {14,7},
+	Symbol = "Fl",
+	Name = "Flerovium",
+	Classification = "Metal"
+}
+
+Elements[115] = {
+	Position = {15,7},
+	Symbol = "Mc",
+	Name = "Moscovium",
+	Classification = "Unknown"
+}
+
+Elements[116] = {
+	Position = {16,7},
+	Symbol = "Lv",
+	Name = "Livermorium",
+	Classification = "Unknown"
+}
+
+Elements[117] = {
+	Position = {17,7},
+	Symbol = "Ts",
+	Name = "Tennessine",
+	Classification = "Unknown"
+}
+
+Elements[118] = {
+	Position = {18,7},
+	Symbol = "Og",
+	Name = "Oganesson",
+	Classification = "Unknown"
 }
